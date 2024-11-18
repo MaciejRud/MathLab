c36c32e77ade9a8f1138644249088f6ee45241e7
MaciejRud <m.rudol93@gmail.com>
2024-11-18 10:49:15 +0100

    Configuration alembic and add model Teacher.

--- MathLab/models/model.py
+++ MathLab/models/model.py
@@ -2,7 +2,31 @@
 Databases models for Api.
 '''
 
+import uuid
+
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    UUID,
+    Date,
+    Boolean,
+    Table,
+    Enum,
+)
+
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+
+class Teacher(Base):
+    __tablename__ = 'teachers'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    email = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+    name = Column(String)
+    surname = Column(String)
+    disabled = Column(Boolean, default=False)
